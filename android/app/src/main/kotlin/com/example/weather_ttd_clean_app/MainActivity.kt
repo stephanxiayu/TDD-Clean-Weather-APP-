f7909921bbba83da3feb8304688119ef419ad9e9
Stephan Granzow <stephan_granzow@web.de>
2024-07-09 18:15:37 +0200

    first

---
+++ android/app/src/main/kotlin/com/example/weather_ttd_clean_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weather_ttd_clean_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("vielleicht werde ich mal Cool"),
+      ),
+    );
+  }
+}
