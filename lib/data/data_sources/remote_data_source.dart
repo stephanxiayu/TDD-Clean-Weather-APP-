8befc7f375c5fead37818467c243df7e161a2999
Stephan Granzow <stephan_granzow@web.de>
2024-07-15 16:54:52 +0200

    5 tests

---
+++ lib/data/data_sources/remote_data_source.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:weather_ttd_clean_app/core/constant/constants.dart';
+import 'package:weather_ttd_clean_app/core/error/exception.dart';
+import 'package:weather_ttd_clean_app/data/models/weather_model.dart';
+import "package:http/http.dart" as http;
+
+abstract class WeatherRemoteDataSource {
+  Future<WeatherModel> getCurrentWeather(String cityName);
+}
+
+class WeatherRemoteDataSourceImpl extends WeatherRemoteDataSource {
+  final http.Client client;
+  WeatherRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<WeatherModel> getCurrentWeather(String cityName) async {
+    final response =
+        await client.get(Uri.parse(Urls.currentWeatherByName(cityName)));
+
+    if (response.statusCode == 200) {
+      return WeatherModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+}
---
+++ test/data/data_sources/remote_data_source_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_ttd_clean_app/core/constant/constants.dart';
+import 'package:weather_ttd_clean_app/data/data_sources/remote_data_source.dart';
+import "package:http/http.dart" as http;
+import 'package:weather_ttd_clean_app/data/models/weather_model.dart';
+import '../../helpers/json_reader.dart';
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late MockHttpClient mockHttpClient;
+
+  late WeatherRemoteDataSourceImpl weatherRemoteDataSourceImpl;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    weatherRemoteDataSourceImpl =
+        WeatherRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  const testCityName = "New York";
+  group("get current weather", () {
+    test("should return weather model when the response is 200", () async {
+      //arrage
+      when(mockHttpClient
+              .get(Uri.parse(Urls.currentWeatherByName(testCityName))))
+          .thenAnswer((_) async => http.Response(
+              readJson("helpers/dummy_data/dummy_weather_response.json"), 200));
+
+      //act
+      final result =
+          await weatherRemoteDataSourceImpl.getCurrentWeather(testCityName);
+      //assert
+
+      expect(result, isA<WeatherModel>());
+    });
+  });
+}
